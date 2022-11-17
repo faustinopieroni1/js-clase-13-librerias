34e3ba612ea000db38d0a6e9a5fc2482b34e89ca
faustinopieroni1 <fauspieroni@gmail.com>
2022-11-17 19:24:15 -0300

    x

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,42 @@ function aletra() {
     })
 }
 
+//--------------------------------------------
 
+let btn2 = document.getElementById("btn2")
+
+btn2.addEventListener("click", notificacion)
+
+function notificacion() {
+
+   /*  Toastify({
+        text: "This is a toast",
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "left", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+        onClick: function () { } // Callback after click
+    }).showToast(); */
+    
+    Toastify({
+        text: "Hiciste click",
+        className: "info",
+        style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+        }
+    }).showToast();
+
+    /* Toastify({
+        text: "This is a toast with offset",
+        offset: {
+          x: 50, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+          y: 10 // vertical axis - can be a number or a string indicating unity. eg: '2em'
+        },
+      }).showToast(); */
+}
